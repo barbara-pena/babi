1e51f4995964bb3df44847835544c960be9d50b3
Bárbara Talita <100386885+barbara-pena@users.noreply.github.com>
2022-03-03 19:57:08 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+    $ git clone <insira_a_url_do_projeto> [comando para clonar os projetos]
+    $ cd <nome_da_pasta_do_projeto> [comando para entrar na pasta do projeto]
+    $ git config --global user.name "<seu_nome>" [comando para adicionar suas credenciais - nome]
+    $ git config --global user.email "seu_e-mail" [comando para adicionar suas credenciais - email]
+    $ git add . [comando para adicionar as alterações]
+    $ git commit -m "comentário sobre as alteracoes feitas" [comando confirmar as alterações feitas]
+    $ git pull [comando para puxar a última versao do código]
+    $ git push -u [comando para empurrar sua branch para o repositório remoto] / OU / $ git push --set-upstream origin <sua_branch> [somente no primeiro commit]
